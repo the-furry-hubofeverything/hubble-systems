ee24b813be8935786f8609880b30c50da4163b19
the-furry-hubofeverything <53921912+the-furry-hubofeverything@users.noreply.github.com>
2023-10-13 00:53:39 -0700

    pc: add unifont, noto-fonts, liberation_ttf

--- hosts/pc/common.nix
+++ hosts/pc/common.nix
@@ -40,6 +40,14 @@
     xkbVariant = "";
   };
 
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    liberation_ttf
+    unifont
+  ];
+
   # === NIX SETTINGS ===
   nixpkgs = {
     overlays = [
