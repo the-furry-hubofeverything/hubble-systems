5565a859ad55537b14ba7ee90734dd53e9dc21f8
the-furry-hubofeverything <53921912+the-furry-hubofeverything@users.noreply.github.com>
2023-10-17 19:01:16 -0700

    pc: temporarily fix bluetooth problems
    TODO - play around settings to see which borks it

--- hosts/pc/common/desktop.nix
+++ hosts/pc/common/desktop.nix
@@ -39,16 +39,16 @@
   ];
 
   # Better codecs for Bluetooth devices
-  environment.etc = {
-    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-      bluez_monitor.properties = {
-        ["bluez5.enable-sbc-xq"] = true,
-        ["bluez5.enable-msbc"] = true,
-        ["bluez5.enable-hw-volume"] = true,
-        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-      }
-    '';
-  };
+  # environment.etc = {
+  #   "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+  #     bluez_monitor.properties = {
+  #       ["bluez5.enable-sbc-xq"] = true,
+  #       ["bluez5.enable-msbc"] = true,
+  #       ["bluez5.enable-hw-volume"] = true,
+  #       ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+  #     }
+  #   '';
+  # };
 
   # Logitech Wireless peripheral udev support (including unify)
   hardware.logitech.wireless.enable = true;
