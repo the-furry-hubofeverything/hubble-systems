4eca1333a5b35e905a0f620e3adb2fa448581d20
the-furry-hubofeverything <53921912+the-furry-hubofeverything@users.noreply.github.com>
2023-10-18 23:48:21 -0700

    quicksand: correct hash

--- pkgs/quicksand/default.nix
+++ pkgs/quicksand/default.nix
@@ -8,7 +8,7 @@ stdenvNoCC.mkDerivation {
     owner = "andrew-paglinawan";
     repo = "QuicksandFamily";
     rev = "be4b9d638e1c79fa42d4a0ab0aa7fe29466419c7";
-    hash = "sha256-0a4jNjhe2BCQzM/NVjiS7H5pzdtCRThI8nseob+My+w=";
+    hash = "sha256-zkxm2u35Ll2qyCoUeuA0eumVjNSel+y1kkWoHxeNI/g=";
     sparseCheckout = ["fonts"];
   };
 
