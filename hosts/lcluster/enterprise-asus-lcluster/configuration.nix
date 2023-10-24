fdecf7682f7fedc304db39fca26bdb783e5de945
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2023-10-24 00:04:20 -0700

    lcluster: init enterprise-asus-lcluster/configuration.nix

--- hosts/lcluster/enterprise-asus-lcluster/configuration.nix
+++ hosts/lcluster/enterprise-asus-lcluster/configuration.nix
@@ -2,4 +2,12 @@
   imports = [
     ../common.nix
   ];
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = ["btrfs"];
+
+  networking.hostName = "enterprise-asus-lcluster"; # Define your hostname.
+
+  system.stateVersion = "23.05";
 }
