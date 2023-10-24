9c6c383c61244494817165e68cea48d72fad4a0d
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2023-10-23 20:27:47 -0700

    minecraftServers: added various performance improvements
    added arguments via https://flags.sh/
    added new mods to fabric-server-optimizations
    removed fabric api to fane-train-shenanigans because of conflict
    edited server.properties to the creative server

--- hosts/lcluster/titan-razer-lcluster/minecraftServers/default.nix
+++ hosts/lcluster/titan-razer-lcluster/minecraftServers/default.nix
@@ -7,17 +7,40 @@
   ...
 }: let
   fabricServerOptimizations = pkgs.fetchPackwizModpack {
-    url = "https://github.com/the-furry-hubofeverything/fabric-server-optimizations/raw/a4d9559ceeac6657443f21a599ce7d22e080ad6f/pack.toml";
-    packHash = "sha256-E/kChdScTNEuonlswl59T1U7LdJ9iADMfWSQJs+oqUk=";
+    url = "https://github.com/the-furry-hubofeverything/fabric-server-optimizations/raw/f88e977440507469023fc9836aaf245e71bf802f/pack.toml";
+    packHash = "sha256-bd/bzEMiryRP6yp2A126KweBw+LvdoZ4ijHh91WrMmQ=";
   };
   fanesTrainShenanigans = pkgs.fetchPackwizModpack {
-    url = "https://github.com/the-furry-hubofeverything/fanes-train-shenanigans/raw/599f90a85c95e43b4631d1b5221e8e57bf6ab816/pack.toml";
-    packHash = "sha256-MOyP6/X3Mwl4ONX7DZJYM+0pu3wPKiBYsMFdF6SoaWE=";
-  }; 
+    url = "https://github.com/the-furry-hubofeverything/fanes-train-shenanigans/raw/8a569e29b44a36d63a4f46eb373477b524091fd2/pack.toml";
+    packHash = "sha256-wHgS3JJlTiSesYuKdXDm0hcUepTO2gXSp4b0vqqmJEA=";
+  };
 
   modpack = fabricServerOptimizations.addFiles {
     "" = "${fanesTrainShenanigans}/mods";
   };
+
+  jvmOptimizationFlags = ''
+    --add-modules=jdk.incubator.vector \
+    -XX:+UseG1GC \
+    -XX:+ParallelRefProcEnabled \
+    -XX:MaxGCPauseMillis=200 \
+    -XX:+UnlockExperimentalVMOptions \
+    -XX:+DisableExplicitGC \
+    -XX:+AlwaysPreTouch \
+    -XX:G1HeapWastePercent=5 \
+    -XX:G1MixedGCCountTarget=4 \
+    -XX:InitiatingHeapOccupancyPercent=15 \
+    -XX:G1MixedGCLiveThresholdPercent=90 \
+    -XX:G1RSetUpdatingPauseTimePercent=5 \
+    -XX:SurvivorRatio=32 \
+    -XX:+PerfDisableSharedMem \
+    -XX:MaxTenuringThreshold=1 \
+    -Dusing.aikars.flags=https://mcflags.emc.gs \
+    -Daikars.new.flags=true -XX:G1NewSizePercent=30 \
+    -XX:G1MaxNewSizePercent=40 \
+    -XX:G1HeapRegionSize=8M \
+    -XX:G1ReservePercent=20 \
+    '';
 in {
   nixpkgs.overlays = [
     inputs.nix-minecraft.overlay
@@ -35,6 +58,7 @@ in {
       "SMP" = {
         enable = true;
         package = pkgs.paperServers.paper;
+        jvmOpts = jvmOptimizationFlags;
 
         # TODO declarative whitelist
         serverProperties = {
@@ -50,7 +74,7 @@ in {
       "creative" = {
         enable = true;
         package = pkgs.fabricServers.fabric-1_19_4;
-        jvmOpts = "-Xmx8G -Xms1G";
+        jvmOpts = jvmOptimizationFlags + "-Xmx8G -Xms8G";
 
         serverProperties = {
           gamemode = "creative";
@@ -58,7 +82,9 @@ in {
           spawn-monsters = false;
           white-list = true;
           server-port = 25566;
-          view-distance = 16;
+          sync-chunk-writes = false;
+          simulation-distance = 6;
+          network-compression-threshold = 256;
 
           op-permission-level = 2;
         };
