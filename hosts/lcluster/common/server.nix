0e20e6812022db7a73a97a08f8c2d9902837072f
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2023-11-06 01:47:47 -0800

    lcluster: removed nomodeset and vga kernel params
    I want my screen actually

--- hosts/lcluster/common/server.nix
+++ hosts/lcluster/common/server.nix
@@ -39,7 +39,7 @@
   nix.settings.trusted-users = ["@wheel" "hubble"];
 
   # Since we can't manually respond to a panic, just reboot.
-  boot.kernelParams = ["panic=1" "boot.panic_on_fail" "vga=0x317" "nomodeset"];
+  boot.kernelParams = ["panic=1" "boot.panic_on_fail"];
 
   # Don't allow emergency mode, because we don't have a console.
   systemd.enableEmergencyMode = false;
