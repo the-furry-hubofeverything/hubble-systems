9cdb3b3d97182d3907cd83281c8fccdc8f1a18a5
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2023-11-13 22:23:47 -0800

    lcluster: let sops-nix use persist ssh key instead of root filesystem
    fixes #23

--- hosts/lcluster/common/security.nix
+++ hosts/lcluster/common/security.nix
@@ -11,6 +11,9 @@
   sops.secrets.hubblePasswd.neededForUsers = true;
   sops.secrets.rootPasswd.neededForUsers = true;
 
+  sops.gnupg.sshKeyPaths = [];
+  sops.age.sshKeyPaths = [ "/persist/etc/ssh/ssh_host_ed25519_key"];
+
   users.users = {
     "hubble".passwordFile = config.sops.secrets.hubblePasswd.path;
     "root".passwordFile = config.sops.secrets.rootPasswd.path;
