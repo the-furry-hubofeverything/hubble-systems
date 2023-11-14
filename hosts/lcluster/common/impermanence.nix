14305c6bb787877f4be19dbd0e94dfb7c091ae1b
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2023-11-13 22:45:12 -0800

    lcluster: move impermanence related sops settings to impermanence.nix

--- hosts/lcluster/common/impermanence.nix
+++ hosts/lcluster/common/impermanence.nix
@@ -26,4 +26,6 @@
     "/var/lib/logrotate.status"
   ];
  }; 
+
+  sops.age.sshKeyPaths = [ "/persist/etc/ssh/ssh_host_ed25519_key" ];
 }
--- hosts/lcluster/common/security.nix
+++ hosts/lcluster/common/security.nix
@@ -12,7 +12,6 @@
   sops.secrets.rootPasswd.neededForUsers = true;
 
   sops.gnupg.sshKeyPaths = [];
-  sops.age.sshKeyPaths = [ "/persist/etc/ssh/ssh_host_ed25519_key"];
 
   users.users = {
     "hubble".passwordFile = config.sops.secrets.hubblePasswd.path;
