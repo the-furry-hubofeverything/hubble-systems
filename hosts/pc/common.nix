beb168ea5b2d2d82d2bc3f8db5c1b0f3d194254c
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2023-12-03 14:30:34 -0800

    pc: remove hyprland substituter

--- hosts/pc/common.nix
+++ hosts/pc/common.nix
@@ -50,11 +50,9 @@
   nix = {
     settings = {
       substituters = [
-        "https://hyprland.cachix.org"
         "https://cuda-maintainers.cachix.org"
       ];
       trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       ];
     };
