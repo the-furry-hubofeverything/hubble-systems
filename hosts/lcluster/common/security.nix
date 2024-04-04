acd989707b0529843473c0c18ea8f97100e10947
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2024-04-03 19:43:07 -0700

    lcluster: set default password for lcluster-common
    Working towards #24 and #9.
    This took a while, but I think I got it.
    It's still kind of hacky but, I think I've covered my butt.
    it's currently set to ignore hs-secrets ONLY IF it's on *-common.
    When it does ignore hs-secrets, it warns the user during build.

--- hosts/lcluster/common/security.nix
+++ hosts/lcluster/common/security.nix
@@ -1,4 +1,34 @@
-{ config, ... }: {
+{
+  config,
+  lib,
+  hs-utils,
+  ...
+}: let
+  mkPassword = secret: fallback: user: {
+    # Checks if secret is set, if not use fallback as password.
+    hashedPasswordFile = if hs-utils.sops.isDefault config.sops secret
+      then lib.warn "Password secret not imported for the user ${user}, setting path to null" null
+      else config.sops.secrets.${secret}.path;
+
+    password = if hs-utils.sops.isDefault config.sops secret
+      then lib.warn "Debug password for ${user} set: ${fallback}" fallback
+      else null;
+  };
+
+  mkAssertion = secret: user: {
+    # If any secrets are undefined that is NOT on *-common configuration
+    assertion = !hs-utils.sops.isDefault config.sops secret != (builtins.match "(.*)-common" config.networking.hostName);
+    message = "security: password for ${user} not defined on configuration, cannot continue";
+  };
+in {
+  assertions = [
+    {
+      assertion = hs-utils.sops.defaultIsEmpty config.sops;
+      message = "security: defaultSopsFile not empty, cannot continue";
+    }
+    (mkAssertion "hubblePasswd" "hubble")
+    (mkAssertion "rootPasswd" "root")
+  ];
   security.lockKernelModules = true;
 
   fileSystems."/".options = [ "noexec" ];
@@ -15,7 +45,7 @@
   sops.gnupg.sshKeyPaths = [];
 
   users.users = {
-    "hubble".hashedPasswordFile = config.sops.secrets.hubblePasswd.path;
-    "root".hashedPasswordFile = config.sops.secrets.rootPasswd.path;
+    "hubble" = mkPassword "hubblePasswd" "tank-weasel" "hubble";
+    "root" = mkPassword "rootPasswd" "hubble-systems" "root";
   };
 }
