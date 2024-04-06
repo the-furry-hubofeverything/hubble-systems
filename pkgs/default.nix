aee512e36541c2005d6d476a27076785e16bd8c1
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2024-04-06 01:50:03 -0700

    wii-pointers: update to 2024-04-02
    Now with multiplayer!

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,11 +1,12 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
-pkgs: {
+pkgs: rec {
   # example = pkgs.callPackage ./example { };
   beammp-server = pkgs.callPackage ./beammp-server/default.nix {};
   flamenco = pkgs.callPackage ./flamenco/default.nix {};
   koulen = pkgs.callPackage ./koulen/default.nix {};
   sheepit-client = pkgs.callPackage ./sheepit-client/default.nix {};
-  wii-pointer = pkgs.callPackage ./wii-pointer/default.nix {};
+  wii-pointer = wii-pointers.p1;
+  wii-pointers = pkgs.callPackage ./wii-pointer/default.nix {};
   # quicksand = pkgs.callPackage ./quicksand/default.nix {};
 }
--- pkgs/wii-pointer/default.nix
+++ pkgs/wii-pointer/default.nix
@@ -5,33 +5,39 @@
   p7zip,
   ... 
 }: 
+let 
+  build = player: stdenvNoCC.mkDerivation {
+    name = "wii-pointer";
+    version = "2024-04-02";
 
-stdenvNoCC.mkDerivation {
-  name = "wii-pointer";
-  version = "2024-03-31";
+    src = fetchurl {
+      name = "linux-cursors.7z";
+      url = "https://files.primm.gay/extras/cursors/Wii/Linux%20Cursors.7z";
+      hash = "sha256-ATEERNrI0zR6ZL57cL39P+2Mu/Qf2CaQjksCWZSVia8=";
+    };
 
-  src = fetchurl {
-    name = "linux-cursors.7z";
-    url = "https://files.primm.gay/extras/cursors/Wii/Linux%20Cursors.7z";
-    hash = "sha256-1lFJrLYEyT1STLgK1YOpy/g4tgGk/ENnri5QjR0dMzo=";
+    unpackPhase = ''
+      ${p7zip}/bin/7z x $src
+    '';
+
+    installPhase = ''
+      install -dm 755 $out/share/icons
+      cp -dr --no-preserve='ownership' 'Linux Cursor/Wii-Pointer-P${player}' $out/share/icons/
+    '';
+
+    meta = with lib; {
+      description = "Wii pointer cursor theme (Player ${player})";
+      homepage = "https://primm.gay/extras/other/cursors/";
+      # From README.txt - Feel free to use this set for whatever you want, but please seek my permission if you wish to redistribute it.
+      # Since there's no mention of a specific license, I'm going to set this to unfree
+      license = licenses.unfree;
+      platforms = platforms.all;
+      maintainers = with maintainers; [ hubble ];
+    };
   };
-
-  unpackPhase = ''
-    ${p7zip}/bin/7z x $src
-  '';
-
-  installPhase = ''
-    install -dm 755 $out/share/icons
-    cp -dr --no-preserve='ownership' 'Linux Cursor/Wii-Pointer' $out/share/icons/
-  '';
-
-  meta = with lib; {
-    description = "Wii pointer cursor theme";
-    homepage = "https://primm.gay/extras/other/cursors/";
-    # From README.txt - Feel free to use this set for whatever you want, but please seek my permission if you wish to redistribute it.
-    # Since there's no mention of a specific license, I'm going to set this to unfree
-    license = licenses.unfree;
-    platforms = platforms.all;
-    maintainers = with maintainers; [ hubble ];
-  };
+in {
+  p1 = build "1";
+  p2 = build "2";
+  p3 = build "3";
+  p4 = build "4";
 }
