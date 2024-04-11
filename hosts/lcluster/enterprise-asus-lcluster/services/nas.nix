9e87ce606764d014861e1519d973929455559f7f
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2024-04-10 19:28:00 -0700

    nas: readjust mount points
    Maybe this fixes the "mount point does not exist" problem

--- hosts/lcluster/enterprise-asus-lcluster/services/nas.nix
+++ hosts/lcluster/enterprise-asus-lcluster/services/nas.nix
@@ -12,20 +12,21 @@ in {
     "main"
   ];
 
-  fileSystems."/run/media/hubble/store0" = {
+  # Just a bunch of partitions
+  fileSystems."/jbop0" = {
     device = "/dev/disk/by-partuuid/11a18715-08dd-4b57-9f20-0f5396cba3b2";
     fsType = "ext4";
     options = ["rw" "user" "exec" "errors=remount-ro" "nofail"];
   };
 
-  fileSystems."/run/media/hubble/store1" = {
+  fileSystems."/jbop1" = {
     device = "/dev/disk/by-partuuid/cef73c67-c122-4943-905a-10b3bf9e62bf";
     fsType = "ext4";
     options = ["rw" "user" "exec" "errors=remount-ro" "nofail"];
   };
 
   fileSystems."/mass" = { 
-    device = "/run/media/hubble/store*";
+    device = "/jbop*";
     fsType = "mergerfs";
     options = ["defaults" "minfreespace=250G" "cache.files=partial" "dropcacheonclose=true" "category.create=mfs"];
   };
