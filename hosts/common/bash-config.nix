b6cb14cc4217f027a05f155d46a7b50c0ffdf5b7
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2024-04-18 11:29:35 -0700

    bash-config: turn off undistract me
    It never pings when I needed it to and always pings when I don't

--- hosts/common/bash-config.nix
+++ hosts/common/bash-config.nix
@@ -3,14 +3,6 @@
     trashy
   ];
 
-  programs.bash = {
-    undistractMe = {
-      enable = true;
-      playSound = true;
-      timeout = 90;
-    };
-  };
-
   environment.shellAliases = {
     rm = "echo -e 'warning: use of rm discouraged. Either use trashy or bypass this warning using \\\\rm'";
   };
