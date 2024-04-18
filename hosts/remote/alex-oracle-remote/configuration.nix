8820f6719b3e81ddea3ec7e573e8aa5598e99633
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2024-04-18 15:26:14 -0700

    alex-oracle-remote: limit stored generations
    Not a lot of disk space :P

--- hosts/remote/alex-oracle-remote/configuration.nix
+++ hosts/remote/alex-oracle-remote/configuration.nix
@@ -9,6 +9,7 @@
 
   # Cloud console debug
   boot.kernelParams = ["console=ttyS0,9600"];
+  boot.loader.systemd-boot.configurationLimit = 3;
 
   networking.hostName = "alex-oracle-remote"; # Define your hostname.
   networking.domain = "subnet11062027.vcn11062027.oraclevcn.com";
