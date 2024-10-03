6956a1e4840bb77706276ff3414ffb33823b4528
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2024-10-03 03:52:25 -0700

    gaming: change pam nofile hard limit for lutris

--- hosts/pc/common/gaming.nix
+++ hosts/pc/common/gaming.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   programs.gamemode = {
     enable = true;
     settings.general.renice = 10;
@@ -9,6 +13,15 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
+
+  security.pam.loginLimits = lib.mkForce [
+    {
+      domain = "users";
+      item = "nofile";
+      type = "hard";
+      value = "524288";
+    }
+  ];
 
   environment.systemPackages = [
     pkgs.protonup-ng
