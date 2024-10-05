69c2e8793c2b830e0de0f64c0cc3ddfb819fda29
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2024-10-05 14:22:49 -0700

    gulo-laptop: attempt to fix kvm issue

--- hosts/pc/gulo-laptop/configuration.nix
+++ hosts/pc/gulo-laptop/configuration.nix
@@ -43,16 +43,7 @@
     # Don't use the open source kernel module, due to its incompatiblity with the SVM fix
     open = true;
     nvidiaSettings = false;
-    # Use R560 to overcome the external monitor kernel panics
-    # (latest beta as of 2024-08-20) 
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "560.31.02";
-      sha256_64bit = "sha256-0cwgejoFsefl2M6jdWZC+CKc58CqOXDjSi4saVPNKY0=";
-      sha256_aarch64 = "sha256-m7da+/Uc2+BOYj6mGON75h03hKlIWItHORc5+UvXBQc=";
-      openSha256 = "sha256-X5UzbIkILvo0QZlsTl9PisosgPj/XRmuuMH+cDohdZQ=";
-      settingsSha256 = "sha256-A3SzGAW4vR2uxT1Cv+Pn+Sbm9lLF5a/DGzlnPhxVvmE=";
-      persistencedSha256 = "sha256-BDtdpH5f9/PutG3Pv9G4ekqHafPm3xgDYdTcQumyMtg=";
-    };
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   # GPU switch
