2d4366b280e46c03e2db1d01e84d7c9efacd3ef6
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2024-12-02 19:44:52 -0800

    obs: update obs to unstable

--- home-manager/hubble/obs.nix
+++ home-manager/hubble/obs.nix
@@ -1,7 +1,8 @@
 {pkgs, ...}: {
   programs.obs-studio = {
     enable = true;
-    plugins = with pkgs; [
+    package = pkgs.unstable.obs-studio;
+    plugins = with pkgs.unstable; [
       obs-studio-plugins.obs-tuna
       obs-studio-plugins.obs-vaapi
       obs-studio-plugins.obs-vkcapture
