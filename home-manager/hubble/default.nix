7d47b79b95bf5a00819cf3f9c06abc0a051719f5
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2024-12-14 02:39:07 -0800

    hubble: Add pinyin input

--- home-manager/hubble/default.nix
+++ home-manager/hubble/default.nix
@@ -106,6 +106,15 @@
     };
   };
 
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-gtk
+      fcitx5-chinese-addons
+      fcitx5-nord
+    ];
+  };
+
   # Bluetooth Media controls
   systemd.user.services.mpris-proxy = {
     Unit.Description = "Mpris proxy";
