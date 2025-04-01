7b6d3f37ea1a4f8161dd6ac5b9f00605672af53a
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2025-03-31 19:08:08 -0700

    bash-config: reconfigure flipcoin to use system entropy

--- hosts/common/bash-config.nix
+++ hosts/common/bash-config.nix
@@ -19,8 +19,9 @@
   environment.shellAliases = {
     rm = "echo -e 'warning: use of rm discouraged. Either use trash-put or bypass this warning using \\\\rm'";
 
-    # Beautiful one liner from https://gist.github.com/theycallmemac/f66b0afeca215df97869dd28612bea74?permalink_comment_id=4376049#gistcomment-4376049
+    # Based on one liner from https://gist.github.com/theycallmemac/f66b0afeca215df97869dd28612bea74?permalink_comment_id=4376049#gistcomment-4376049
     # By srozanc-mcs
-    flipcoin = "(( RANDOM % 2 )) && echo \"heads\" || echo \"tails\"";
+    # Modified to use system entropy
+    flipcoin = "(exit $(shuf -i 0-1 -n 1 --random-source=/dev/urandom)) && echo \"heads\" || echo \"tails\"";
   };
 }
