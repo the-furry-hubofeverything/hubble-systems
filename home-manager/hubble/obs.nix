6ad6ca28468135cab277b04f0a8e7f7b7a9f02ee
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2025-05-01 23:22:20 -0700

    hubble: add workaround for nixos/nixpkgs#400555

--- home-manager/hubble/obs.nix
+++ home-manager/hubble/obs.nix
@@ -6,7 +6,13 @@
       obs-studio-plugins.obs-tuna
       obs-studio-plugins.obs-vaapi
       obs-studio-plugins.obs-vkcapture
-      obs-studio-plugins.looking-glass-obs
+      (obs-studio-plugins.looking-glass-obs.overrideAttrs (final: prev: {
+        nativeBuildInputs =
+          prev.nativeBuildInputs
+          ++ [
+            pkgs.unstable.libGL
+          ];
+      }))
     ];
   };
 }
