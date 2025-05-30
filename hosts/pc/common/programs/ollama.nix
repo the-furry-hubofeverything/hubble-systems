ee187ccbf6d6767fe5a9827dcc67751da72cd00f
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2025-05-29 20:12:42 -0700

    pc: disable ollama on boot

--- hosts/pc/common/programs/ollama.nix
+++ hosts/pc/common/programs/ollama.nix
@@ -1,6 +1,9 @@
-{...}: {
+{lib, ...}: {
   services.ollama = {
     enable = true;
     acceleration = "cuda";
   };
+
+  # Prevents start on boot
+  systemd.services."ollama".wantedBy = lib.mkForce [];
 }
