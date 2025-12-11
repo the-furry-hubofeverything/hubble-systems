2f1fdbaafca0e8d1928afa7133a0659a60af8115
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2025-12-10 16:28:27 -0800

    pc: let's go back to niri's default polkit

--- hosts/pc/common/security.nix
+++ hosts/pc/common/security.nix
@@ -12,20 +12,5 @@
   security.rtkit.enable = true;
   security.polkit.enable = true;
 
-  # Using polkit-gnome
-  systemd.user.services.polkit-gnome-authentication-agent-1 = {
-    description = "polkit-gnome-authentication-agent-1";
-    wantedBy = ["graphical-session.target"];
-    wants = ["graphical-session.target"];
-    after = ["graphical-session.target"];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-      Restart = "on-failure";
-      RestartSec = 1;
-      TimeoutStopSec = 10;
-    };
-  };
-
   security.tpm2.enable = true;
 }
