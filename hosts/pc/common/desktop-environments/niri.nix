01348fafc7b82680d74d03cf14fac478ccc30a6a
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2026-02-22 23:55:52 -0800

    niri: add nvidia app profile

--- hosts/pc/common/desktop-environments/niri.nix
+++ hosts/pc/common/desktop-environments/niri.nix
@@ -35,6 +35,31 @@
     terminal = "kitty";
   };
 
+  environment.etc."nvidia/nvidia-application-profiles-rc.d/50-limit-free-buffer-pool-in-wayland-compositors.json".text = ''
+    {
+      "rules": [
+          {
+              "pattern": {
+                  "feature": "procname",
+                  "matches": "niri"
+              },
+              "profile": "Limit Free Buffer Pool On Wayland Compositors"
+          }
+      ],
+      "profiles": [
+          {
+              "name": "Limit Free Buffer Pool On Wayland Compositors",
+              "settings": [
+                  {
+                      "key": "GLVidHeapReuseRatio",
+                      "value": 0
+                  }
+              ]
+          }
+      ]
+    }
+  '';
+
   environment.variables.NIXOS_OZONE_WL = "1";
   environment.systemPackages = with pkgs; [
     wl-clipboard
