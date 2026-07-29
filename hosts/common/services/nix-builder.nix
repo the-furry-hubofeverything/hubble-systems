652198534dce5ebb7324f6f7c856ea7b10d6bc9c
the-furry-hubofeverything <hubblethewolverine@gmail.com>
2026-07-29 06:31:02 -0700

    nix-builder: remove deployment capabilities

--- hosts/common/services/nix-builder.nix
+++ hosts/common/services/nix-builder.nix
@@ -4,11 +4,10 @@
   lib,
   ...
 }: {
-  users.groups."nixremote" = {};
   users.users."nixremote" = {
     createHome = true;
     isSystemUser = true;
-    group = "nixremote";
+    group = "nogroup";
     shell = pkgs.bash;
     extraGroups = [
       "wheel"
@@ -28,23 +27,6 @@
       PasswordAuthentication no
       X11Forwarding no
   '';
-
-  # Modified from https://github.com/NobbZ/nixos-config/blob/main/nixos/modules/switcher.nix
-  # MIT license, Copyright (c) 2020 Norbert Melzer
-  security.sudo.extraRules = let
-    commandPrefix = "/run/current-system/sw";
-    profilePath = "/nix/store/.{32}-nixos-system-${config.networking.hostName}-.{22}";
-    nixEnvCmd = "${commandPrefix}/bin/nix-env";
-    systemdRunCmd = "${commandPrefix}/bin/env ^NIXOS_INSTALL_BOOTLOADER=[0-1] systemd-run -E LOCALE_ARCHIVE -E NIXOS_INSTALL_BOOTLOADER --collect --no-ask-password --pipe --quiet --service-type=exec --unit=nixos-rebuild-switch-to-configuration";
-    options = ["NOPASSWD"];
-    mkRule = command: {
-      commands = [{inherit command options;}];
-      groups = ["nixremote"];
-    };
-  in [
-    (mkRule "${systemdRunCmd} ${profilePath}/bin/switch-to-configuration (switch|boot|test)$")
-    (mkRule "${nixEnvCmd} ^-p /nix/var/nix/profiles/system --set ${profilePath}$")
-  ];
 
   services.nebula.networks."hsmn0".firewall.inbound =
     lib.optionals config.services.nebula.networks."hsmn0".enable
